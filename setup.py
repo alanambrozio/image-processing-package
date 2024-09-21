c71f251da0590decff93387a479ae53d81fac730
buzinadeaviao <ambrozioalan@gmail.com>
2024-09-20 21:14:17 -0300

    alterando projeto

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='image_processingtest',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Seu Nome',
+    author_email='seuemail@dominio.com',
+    description='Cópia do processamento de imagem para projeto',
+    url='https://github.com/alanambrozio/image-processing-packagetest',
+)
